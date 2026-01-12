fbed08d701155e23b18b9f820039ce05ae4cef18
Mark Andre <markandrevega@me.com>
2026-01-12 22:09:13 +0100

    data and vidfeo from imdb

--- app/components/IndexIntro.vue
+++ app/components/IndexIntro.vue
@@ -22,20 +22,8 @@ onMounted(() => {
 </script>
 <template>
   <div class="video-container">
-    <div v-if="isLoading" class="loader">
-      Loading Reel...
-    </div>
-
-    <div v-else-if="featuredVideo" class="video-wrapper">
-			<video :src="featuredVideo.video_url" :poster="featuredVideo.image" autoplay muted loop playsinline class="main-video"></video>
-      
-      <div class="video-credit">
-        Video by {{ featuredVideo.user }}
-      </div>
-    </div>
-
-    <div v-else class="error">
-      Failed to load video.
+    <div class="video-wrapper">
+      <video class="main-video" autoplay muted playsinline title="" src="https://imdb-video.media-imdb.com/vi2536814873/1434659607842-pgv4ql-1501028571775.mp4?Expires=1768337488&Signature=kPTvJQ84syEbaApt608KNwIcsyhwj-1AXojjBh~mty2GOgeXHVfD7t~KwPIW7-uXIn9p~17HtbibSda-m9J5NTlOt2G-aVXr3X94riRt3XcPHF0UyY7T9o5wyRGBuGyqWs9FbYHe4A~EYqH7d-wxaQGZyCHpBqcmYuxeQK5Hps6B8OJaUzB0q~GuVqJqnfIlspykY~kfHYl9sqgiQIi4VMDM30fQmF19pxclf8LNEklePfJ6As1f1YsXxddMCc55Id7prWYQTSWhEOiv15CGnpsQS4l5yzyXBIOwLuK4YajgeXyWtnlFl7X~YR6Tpvrp1UetVLenO8eR9lZ-lx5MSw__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA"></video>
     </div>
   </div>
 </template>
