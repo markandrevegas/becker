8f322cd54b9ebce91fe3c3ef7013a39daeaeac2d
Mark Andre <markandrevega@me.com>
2026-01-17 15:46:10 +0100

    desktop footer

--- app/components/IndexFooter.vue
+++ app/components/IndexFooter.vue
@@ -2,15 +2,16 @@
 <template>
 	<div>
 		<footer class="flex min-h-72 flex-col gap-8 bg-abyssal px-8 pb-16 pt-24 text-sm text-white">
-			<div class="md:w-4/5 mx-auto">
+			<div class="md:w-4/5 mx-auto text-lg">
+				<h2 class="mt-2 mb-8 w-full max-w-md text-2xl text-3xl font-medium uppercase sm:text-4xl">Management</h2>
 				<div class="grid grid-cols-2 gap-16 md:grid-cols-3 md:gap-0 ">
 					<div class="col-span-2 flex flex-col md:col-span-1">
-						<span class="mb-2 block font-bold text-slate-500">Evolve Talent Management</span>
+						<span class="mb-2 block font-semibold text-slate-400 dark:text-yellow-50/50">Evolve Talent Management</span>
 						<span>Sandra Berney</span>
 						<a href="mailto:Sandra.evolvetalent@gmail.com" class="hover:underline">sandra.evolvetalent@gmail.com</a>
 					</div>
-					<div class="col-span-2 flex flex-col md:col-span-1">
-						<span class="mb-2 block font-bold text-slate-500">Evolve Talent Management COMM-Henderson Represents Inc. (HRI)</span>
+					<div class="col-span-2 flex flex-col">
+						<span class="mb-2 block font-semibold text-slate-400 dark:text-yellow-50/50">COMM-Henderson Represents Inc. (HRI)</span>
 						<span>Michelle Henderson</span>
 						<a mailto="mhenderson@hritalent.com" class="hover:underline">mhenderson@hritalent.com</a>
 						<span>310-295-077</span>
