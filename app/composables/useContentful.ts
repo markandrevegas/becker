09b00bb2bd1c2bd23c9c89675ceb8fc2d4ed20f8
mark andre stephens <127994059+markandrevegas@users.noreply.github.com>
2026-02-02 13:36:35 +0100

    deploy fix

--- app/composables/useContentful.ts
+++ app/composables/useContentful.ts
@@ -1,26 +1,23 @@
 // composables/useContentful.ts
 import { createClient } from 'contentful'
-import type { OnePager } from '~/types/contentful'
-import { parseOnePager } from '../utils/contentfulParser'
 
 export const useContentful = () => {
   const config = useRuntimeConfig()
-  const spaceId = config.public.contentfulSpaceId as string
-  const accessToken = config.public.contentfulAccessToken as string
+  const spaceId = config.public.contentfulSpaceId
+  const accessToken = config.public.contentfulAccessToken
 
   if (!spaceId || !accessToken) {
     throw new Error('Contentful configuration missing. SpaceId: '+ !!spaceId + 'Token: ' + !!accessToken)
   }
 
   const client = createClient({
-    space: spaceId,
-    accessToken: accessToken
+    space: spaceId || '',
+    accessToken: accessToken || ''
   })
   const getOnePager = (id: string) => {
-    return useAsyncData<OnePager>('contentful-' + id, async () => {
+    return useAsyncData('contentful-' + id, async () => {
       const entry = await client.getEntry(id)
-      if (!entry) throw new Error('Entry' + id + ' not found')
-      return parseOnePager(entry)
+      return entry
     })
   }
 
