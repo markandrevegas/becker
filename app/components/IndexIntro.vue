7db705428100e9dd76b0038dac8b4881a861355f
mark andre stephens <127994059+markandrevegas@users.noreply.github.com>
2026-02-03 09:46:38 +0100

    number test for image

--- app/components/IndexIntro.vue
+++ app/components/IndexIntro.vue
@@ -9,12 +9,12 @@
 	const IntroParagraph = computed(() => onePager.value?.aboutParagraph ?? '')
 
 	interface GalleryImage {
-		id: string
+		id: number
 		src: string
 		alt: string
 	}
 
-	const images: GalleryImage[] = [{ id: '4', src: "/assets/gallery/5.webp", alt: "On-set Behind the Scenes" }]
+	const images: GalleryImage[] = [{ id: 4, src: "/assets/gallery/5.webp", alt: "On-set Behind the Scenes" }]
 </script>
 <template>
 	<div class="flex flex-col gap-4 lg:flex-row sm:flex-row-reverse items-start md:items-center">
