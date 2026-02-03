e360c1746ab326667ee03bcfef942bac37333ce7
mark andre stephens <127994059+markandrevegas@users.noreply.github.com>
2026-02-03 11:14:33 +0100

    started type scale

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,19 @@ const config: Config = {
         sans: ["Jost", "sans-serif"],
         display: ["Corinthia", "serif"]
       },
+      fontSize: {
+        "body-sm": ["var(--fs-body-sm)", { lineHeight: "var(--lh-normal)" }],
+        body: ["var(--fs-body)", { lineHeight: "var(--lh-relaxed)" }],
+        "body-lg": ["var(--fs-body-lg)", { lineHeight: "var(--lh-relaxed)" }],
+
+        "h-sm": ["var(--fs-h-sm)", { lineHeight: "var(--lh-snug)" }],
+        "h-md": ["var(--fs-h-md)", { lineHeight: "var(--lh-snug)" }],
+        "h-lg": ["var(--fs-h-lg)", { lineHeight: "var(--lh-tight)" }],
+
+        "display-sm": ["var(--fs-display-sm)", { lineHeight: "var(--lh-tight)" }],
+        "display-md": ["var(--fs-display-md)", { lineHeight: "var(--lh-tight)" }],
+        "display-lg": ["var(--fs-display-lg)", { lineHeight: "var(--lh-tight)" }],
+      },
       letterSpacing: {
         widest: ".25em"
       },
