0bc283e337724bb720ee4dde1dece872266963bc
mark andre stephens <127994059+markandrevegas@users.noreply.github.com>
2026-02-11 10:33:18 +0100

    footer styles

--- app/components/IndexFooter.vue
+++ app/components/IndexFooter.vue
@@ -45,24 +45,24 @@ const images: GalleryImage[] = [
 
 				<div v-if="images.length === 0" class="mt-4 text-center">// NO_IMAGES_FOUND</div>
 				<div
-					class="absolute right-0 bottom-0 left-0 max-w-wrapper z-30 flex flex-col justify-start px-8 pb-16 text-white dark:text-yellow-50/90"
+					class="absolute right-0 bottom-0 left-0 max-w-wrapper z-30 flex flex-col justify-start px-8 pb-32 text-white dark:text-yellow-50/90"
 				>
 					<h2 class="mt-2 max-w-md font-display text-display-lg drop-shadow-[1px_3px_5px_rgba(0,0,0,0.8)]">
 						{{ agentsHeader }}
 					</h2>
 					<div class="flex w-full flex-col mt-16 gap-16 sm:grid sm:grid-cols-2">
 						<div class="flex flex-col sm:col-span-1">
-							<span class="mb-2 block font-semibold text-slate-200 dark:text-yellow-50/50">Evolve Talent Management</span>
-							<span>Sandra Berney</span>
-							<a href="mailto:Sandra.evolvetalent@gmail.com" class="hover:underline">sandra.evolvetalent@gmail.com</a>
+							<span class="drop-shadow-[1px_3px_5px_rgba(0,0,0,0.8)] mb-2 block font-semibold">Evolve Talent Management</span>
+							<span class="drop-shadow-[1px_3px_5px_rgba(0,0,0,0.8)]">Sandra Berney</span>
+							<a href="mailto:Sandra.evolvetalent@gmail.com" class="hover:underline drop-shadow-[1px_3px_5px_rgba(0,0,0,0.8)]">sandra.evolvetalent@gmail.com</a>
 						</div>
 						<div class="flex flex-col sm:col-span-1">
-							<span class="mb-2 block font-semibold text-slate-200 dark:text-yellow-50/50"
+							<span class="drop-shadow-[1px_3px_5px_rgba(0,0,0,0.8)] mb-2 block font-semibold"
 								>COMM-Henderson Represents Inc. (HRI)</span
 							>
-							<span>Michelle Henderson</span>
-							<a mailto="mhenderson@hritalent.com" class="hover:underline">mhenderson@hritalent.com</a>
-							<span>310-295-077</span>
+							<span class="drop-shadow-[1px_3px_5px_rgba(0,0,0,0.8)]">Michelle Henderson</span>
+							<a mailto="mhenderson@hritalent.com" class="drop-shadow-[1px_3px_5px_rgba(0,0,0,0.8)] hover:underline">mhenderson@hritalent.com</a>
+							<span class="drop-shadow-[1px_3px_5px_rgba(0,0,0,0.8)]">310-295-077</span>
 						</div>
 					</div>
 				</div>
