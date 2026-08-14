1dd6acd85f2e4f3dcf17fc817948c4008c9725a3
mark andre stephens <127994059+markandrevegas@users.noreply.github.com>
2026-08-14 09:37:43 +0200

    nuxt-iamge sizes for gallery

--- app/components/IndexIntro.vue
+++ app/components/IndexIntro.vue
@@ -13,12 +13,15 @@ interface GalleryImage {
 	src: string
 	alt: string
 }
-
+import gallery1 from "~/assets/gallery/1.webp"
+import gallery3 from "~/assets/gallery/3.webp"
+import gallery4 from "~/assets/gallery/4.webp"
+import gallery5 from "~/assets/gallery/5.webp"
 const images: GalleryImage[] = [
-	{ id: "4", src: "/assets/gallery/5.webp", alt: "On-set Behind the Scenes" },
-	{ id: "2", src: "/assets/gallery/1.webp", alt: "On-set Production" },
-	{ id: "6", src: "/assets/gallery/3.webp", alt: "Behind the Scenes" },
-	{ id: "7", src: "/assets/gallery/4.webp", alt: "On-set Action" }
+	{ id: "4", src: gallery1, alt: "On-set Behind the Scenes" },
+	{ id: "2", src: gallery3, alt: "On-set Production" },
+	{ id: "6", src: gallery4, alt: "Behind the Scenes" },
+	{ id: "7", src: gallery5, alt: "On-set Action" }
 ]
 </script>
 <template>
