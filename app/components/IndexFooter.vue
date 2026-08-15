35d368c392bf2a7e1e3208302305081cbbcfe286
mark andre stephens <127994059+markandrevegas@users.noreply.github.com>
2026-08-15 15:17:25 +0200

    footer layout

--- app/components/IndexFooter.vue
+++ app/components/IndexFooter.vue
@@ -5,22 +5,22 @@ const agentsHeader = computed(() => onePager?.value?.fields.agentsHeader ?? "")
 // const agentsInfo = computed(() => onePager?.value?.agentsInfo)
 </script>
 <template>
-	<footer class="mt-auto flex flex-col items-end justify-end gap-8 bg-abyssal text-lg text-white dark:text-yellow-50/90 md:text-xl">
-		<div>
-			<h2 class="mt-2 max-w-md font-display text-display-lg drop-shadow-[1px_3px_5px_rgba(0,0,0,0.8)]">
+	<footer class="mt-auto md:text-xl px-8 pb-24">
+		<div class="px-8">
+			<h2 class="mt-2 max-w-md font-display text-display-lg">
 				{{ agentsHeader }}
 			</h2>
 			<div class="mt-16 flex w-full flex-col gap-16 sm:grid sm:grid-cols-2">
 				<div class="flex flex-col sm:col-span-1">
-					<span class="mb-2 block font-semibold drop-shadow-[1px_3px_5px_rgba(0,0,0,0.8)]">Evolve Talent Management</span>
-					<span class="drop-shadow-[1px_3px_5px_rgba(0,0,0,0.8)]">Sandra Berney</span>
-					<a href="mailto:Sandra.evolvetalent@gmail.com" class="drop-shadow-[1px_3px_5px_rgba(0,0,0,0.8)] hover:underline">sandra.evolvetalent@gmail.com</a>
+					<span class="mb-2 block font-semibold">Evolve Talent Management</span>
+					<span>Sandra Berney</span>
+					<a href="mailto:Sandra.evolvetalent@gmail.com" class="hover:underline">sandra.evolvetalent@gmail.com</a>
 				</div>
 				<div class="flex flex-col sm:col-span-1">
-					<span class="mb-2 block font-semibold drop-shadow-[1px_3px_5px_rgba(0,0,0,0.8)]">COMM-Henderson Represents Inc. (HRI)</span>
-					<span class="drop-shadow-[1px_3px_5px_rgba(0,0,0,0.8)]">Michelle Henderson</span>
-					<a mailto="mhenderson@hritalent.com" class="drop-shadow-[1px_3px_5px_rgba(0,0,0,0.8)] hover:underline">mhenderson@hritalent.com</a>
-					<span class="drop-shadow-[1px_3px_5px_rgba(0,0,0,0.8)]">310-295-077</span>
+					<span class="mb-2 block font-semibold">COMM-Henderson Represents Inc. (HRI)</span>
+					<span>Michelle Henderson</span>
+					<a mailto="mhenderson@hritalent.com" class="hover:underline">mhenderson@hritalent.com</a>
+					<span>310-295-077</span>
 				</div>
 			</div>
 		</div>
