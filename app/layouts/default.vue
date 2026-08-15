3a7d985bb731b281a76dd26c374e452e6dfb4833
mark andre stephens <127994059+markandrevegas@users.noreply.github.com>
2026-08-15 21:48:26 +0200

    positioning and width

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -24,10 +24,10 @@ onBeforeUnmount(() => {
 </script>
 <template>
 	<div>
-		<div ref="scrollContainer" class="relative flex h-screen flex-col overflow-auto" @scroll.passive="onScroll">
+		<div ref="scrollContainer" class="relative flex h-[100dvh] flex-col overflow-auto w-full" @scroll.passive="onScroll">
 			<MainMenuSidebar :is-scrolled="isScrolled" />
 			<!-- Main scroll area -->
-			<div class="flex flex-1 flex-col pt-[70px]">
+			<div class="relative flex flex-1 flex-col pt-[70px] w-full">
 				<slot></slot>
 			</div>
 		</div>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -98,7 +98,7 @@ onMounted(() => {
 <template>
 	<div>
 		<main class="mx-auto flex w-screen flex-1 flex-col">
-			<div v-if="onePager" id="gallery" ref="galleryRef" :class="['transition-opacity delay-500 duration-1000 md:px-8 md:pb-16 md:pt-48', isGalleryVisible ? 'animate-fade-slide-up' : 'opacity-0']">
+			<div v-if="onePager" id="gallery" ref="galleryRef" :class="['transition-opacity delay-500 duration-1000', isGalleryVisible ? 'animate-fade-slide-right' : 'opacity-0']">
 				<IndexGallery />
 			</div>
 			<div v-if="onePager" id="intro" ref="introRef" :class="['transition-opacity delay-500 duration-1000 md:px-8 md:pb-16 md:pt-48', isIntroVisible ? 'animate-fade-slide-up' : 'opacity-0']">
